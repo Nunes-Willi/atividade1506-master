4f81c51b59656f9a85df33d58dc6864958bf11f0
Nunes-Will <nuneswillroberto@gmail.com>
2022-06-25 14:02:31 -0300

    Livraria

---
+++ src/views/HomeView.vue
@@ -0,0 +1,76 @@
+<script></script>
+
+<template>
+  <div class="blocos-superior">
+    <main class="bloco1">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit a nesciunt quas deserunt vero nostrum, impedit earum esse, debitis qui alias vel commodi? Commodi dignissimos possimus adipisci. Totam, fugit.
+    </main>
+
+    <main class="bloco2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit a nesciunt quas deserunt vero nostrum, impedit earum esse, debitis qui alias vel commodi? Commodi dignissimos possimus adipisci. Totam, fugit.
+    </main>
+  </div>
+
+<div class="blocos-inferior">
+  <main class="bloco3">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, porro ducimus. Illum architecto numquam quia explicabo asperiores ex voluptas perferendis nesciunt, ipsam nam sapiente nostrum? Recusandae adipisci esse odit voluptatum.
+  </main>
+  
+  <main class="bloco4">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci, porro ducimus. Illum architecto numquam quia explicabo asperiores ex voluptas perferendis nesciunt, ipsam nam sapiente nostrum? Recusandae adipisci esse odit voluptatum.
+  </main>
+</div>
+</template>
+<style scoped>
+.blocos-superior{
+  padding: 2px;
+}
+.blocos-superior .bloco1{
+  display: flex;
+  float: left;
+  width: 500px;
+  height: 170px;
+  border:3px solid #fed365;
+  border-radius: 10px;
+  margin-top: 10px;
+  margin-left: 100px;
+  background-color: white;
+}
+
+.blocos-superior .bloco2{
+  display: flex;
+  float: right;
+  width: 500px;
+  height: 170px;
+  border:3px solid #fed365;
+  border-radius: 10px;
+  margin-top: 10px;
+  margin-right: 100px;
+  background-color: white;
+}
+
+.blocos-inferior .bloco3{
+  display: flex;
+  float: left;
+  width: 500px;
+  height: 170px;
+  border:3px solid #fed365;
+  border-radius: 10px;
+  margin-top: 90px;
+  margin-left: 100px;
+  background-color: white;
+}
+
+.blocos-inferior .bloco4{
+  display: flex;
+  float: right;
+  width: 500px;
+  height: 170px;
+  border:3px solid #fed365;
+  border-radius: 10px;
+  margin-top: 90px;
+  margin-right: 100px;
+  background-color: white;
+}
+</style>
+
---
+++ src/views/JogosView.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <h1>Jogos</h1>
+  <main>
+    <input type="text" placeholder="insira aqui" />
+    <button>salvar</button>
+    <ul>
+      <li>conteudo</li>
+      <li>conteudo</li>
+      <li>conteudo</li>
+      <li>conteudo</li>
+      <li>conteudo</li>
+    </ul>
+  </main>
+</template>
